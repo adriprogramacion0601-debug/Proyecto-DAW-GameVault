14f82af0a4b1e18c67fc0ac284d89ea6bc8e1dc1
Adrián Domínguez Obrero <adriprogramacion0601@gmail.com>
2026-05-01 02:03:57 +0200

    Endpoints controlador revisar

--- gamevault/src/main/java/com/gamevault/gamevault/controllers/AuthController.java
+++ gamevault/src/main/java/com/gamevault/gamevault/controllers/AuthController.java
@@ -1,8 +1,15 @@
 package com.gamevault.gamevault.controllers;
 
+import com.gamevault.gamevault.models.Usuario;
 import com.gamevault.gamevault.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/gamevault")
@@ -14,5 +21,13 @@ public class AuthController {
         this.service = service;
     }
 
+    @GetMapping("/allUsers")
+    public ResponseEntity<List<Usuario>> allUsers() {
+        return ResponseEntity.ok(service.findAllUsers());
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Usuario>> findById(@RequestBody Long id) {
+        return ResponseEntity.ok(service.findUserById(id));
+    }
 }
